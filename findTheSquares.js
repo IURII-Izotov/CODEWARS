2d9f882080a8ea053e973a28ebb6fcbb12a08611
unknown <iurii.izotov@gmail.com>
2022-01-16 13:07:50 +0300

    add: kata Find tha Squares

--- findTheSquares.js
+++ findTheSquares.js
@@ -1,7 +1,7 @@
 // Problem
 // Complete the
 // function that takes an odd integer(0 < n < 1000000) which is the difference between two consecutive perfect squares, and
-// return these squares as a string in the format "bigger-smaller"
+// return these squares as a string in the format "bigger-smaller"/
 
 // Examples
 // 9-- > "25-16"
